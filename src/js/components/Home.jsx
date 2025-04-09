d06fbe23620b1c58def6a29713d5bfacb9c8c2c4
Irvin Martinez <100785904+Irvin-Mx@users.noreply.github.com>
2025-04-09 18:05:59 +0000

    Complete todo list with fetch

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //Components
 import ToDoList from "./ToDoList.jsx"
@@ -6,10 +6,117 @@ import ToDoList from "./ToDoList.jsx"
 //Component
 const Home = () => {
 
-	const [inputValue, setInputValue] = useState("")
+	const [users, setUsers] = useState([])
 
 	//Array where tasks will be stored
 	const [tasksArray, setTasksArray] = useState([])
+
+
+	const fetchAllUsers = async () => {
+		try {
+			const response = await fetch("https://playground.4geeks.com/todo/users")
+			if (!response.ok) {
+				throw new Error("Something went wrong!")
+			}
+			const data = await response.json()
+			//console.log(data.users)
+			//console.log(data.users)
+			//setUsers([1,2,3,4])
+
+			setUsers(data.users)
+
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
+	//THIS FUNCTION CREATES USERS
+	const createUser = async (userName) => {
+		try {
+			const response = await fetch(`https://playground.4geeks.com/todo/users/${userName}`, {
+				method: "POST",
+				hearders: {
+					"Content-Type": "application/json"
+				}
+			})
+			if (!response.ok) {
+				throw new Error("Something went wrong creating a user!")
+			}
+			const data = await response.json()
+			// console.log(data)
+			setUsers(...users, data)
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
+	//This function retreives usertasks
+	const getUserTasks = async (userName) => {
+		try {
+			const response = await fetch(`https://playground.4geeks.com/todo/users/${userName}`)
+
+			if (!response.ok) {
+				throw new Error("Error in getUserTasks function.")
+			}
+
+			const data = await response.json()
+			// console.log("Below are all the todos")
+			// console.log(data.todos)
+			//setTasksArray(data.todos)
+			setTasksArray(data.todos)
+
+		} catch (e) {
+			console.log("Error that was caugth.", e)
+		}
+	}
+
+	//This function creates nenw posts for users
+	const addNewUserTask = async (userName,task)=>{
+		try{
+			const response = await fetch(`https://playground.4geeks.com/todo/todos/${userName}`,{
+				method: "POST",
+				body: JSON.stringify({label:task,is_done:false}),
+				headers : {
+					"Content-Type": "application/json"
+				}
+			})
+			if(!response.ok){
+				throw new Error("Error in addNewUserTAsk try")
+			}
+		}catch(e){
+			console.log("Error in addNewUserTask function",e)
+		}
+	}
+
+	//Hook
+	useEffect(() => {
+		fetchAllUsers()
+		getUserTasks("user0506")
+	}, [])
+
+	// useEffect(()=>{
+	// 	getUserTasks("user0505")
+	// },[])
+
+	// console.log(users)
+
+	const findUser = users.filter(item => item.name === "user0506")
+
+	// console.log(findUser.length)
+
+	// if (findUser.length == 0) {
+	// // 	//crea al usuario
+	// 	createUser("user0506")
+	// }
+
+	// console.log("here is the end users")
+	// console.log(users)
+	
+	//Get tasks from user
+	//retreiveUserTasks()
+	//console.log()
+
+	const [inputValue, setInputValue] = useState("")
 
 	//Event handlers
 	const handleInput = (e) => {
@@ -19,9 +126,14 @@ const Home = () => {
 		if (e.code === "Enter" && inputValue.length > 0) {
 			//console.log(inputValue)
 			//Add tasks to tasksArray
-			setTasksArray([...tasksArray, inputValue])
+			// setTasksArray([...tasksArray, inputValue])
+			// setInputValue("")
+
+			//Call function that adds a new post to the existing
+			addNewUserTask("user0506", inputValue)
 			setInputValue("")
-		}
+			getUserTasks("user0506")
+		} 
 
 	}
 
@@ -32,7 +144,7 @@ const Home = () => {
 				<input className="p-4 fs-4 fw-light border border-0" onKeyDown={(e) => { handleEnterKey(e) }} value={inputValue} onChange={(e) => { handleInput(e) }} type="text" placeholder="What needs to be done?" />
 			</div>
 
-			<ToDoList className="bg-dark" setTasksArray={setTasksArray} tasks={tasksArray} />
+			<ToDoList setTasksArray={setTasksArray} tasks={tasksArray} getUserTasks={getUserTasks} userName="user0506" />
 		</div>
 	);
 };
--- src/js/components/ToDoList.jsx
+++ src/js/components/ToDoList.jsx
@@ -1,23 +1,43 @@
 import React, { useState } from "react"
 
-const ToDoList = ({ tasks, setTasksArray }) => {
+const ToDoList = ({ tasks, setTasksArray, getUserTasks , userName}) => {
+
+    const deleteTaskById = async (id) => {
+        try{
+            const response = fetch(`https://playground.4geeks.com/todo/todos/${id}`,{
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+
+            if(!response.ok){
+                throw new Error("Could not delete user task")
+            }
+        }catch(e){
+            console.log("Got an error", e)
+        }
+    }
 
     const [deleteButtonVisibility, setDeleteButtonVisibility] = useState(null)
 
     const handleDeleteTask = (e) => {
-        const deletedArray = tasks.filter((item, index, array) => {
-            return item !== array[Number(e.target.id)]
-        })
+        // console.log(e.target.id)
+        deleteTaskById(e.target.id)
+        getUserTasks(userName)
+        // const deletedArray = tasks.filter((item, index, array) => {
+        //     return item !== array[Number(e.target.id)]
+        // })
         //console.log(deletedArray)
-        setTasksArray(deletedArray)
+        //setTasksArray(deletedArray)
     }
 
     return (
         <div className="bg-white w-50">
-            {tasks.length > 0 ? tasks.map((item, index) => (
-                <div onMouseEnter={() => setDeleteButtonVisibility(index)} onMouseLeave={() => setDeleteButtonVisibility(null)} className="bg-white d-flex justify-content-between align-items-center p-4 fs-4 border-bottom" key={index}>
-                    <p className="fw-lighter">{item}</p>
-                    {deleteButtonVisibility == index && <div onClick={(e) => { handleDeleteTask(e) }} id={index} className="text-danger" role="button">x</div>}
+            {tasks.length > 0 ? tasks.map((item) => (
+                <div onMouseEnter={() => setDeleteButtonVisibility(item.id)} onMouseLeave={() => setDeleteButtonVisibility(null)} className="bg-white d-flex justify-content-between align-items-center p-4 fs-4 border-bottom" key={item.id}>
+                    <p className="fw-lighter">{item.label}</p>
+                    {deleteButtonVisibility == item.id && <div onClick={(e) => { handleDeleteTask(e) }} id={item.id} className="text-danger" role="button">x</div>}
                 </div>
             )) : <p className="p-2 fw-lighter">0 tasks in list</p>}
             {tasks.length > 0 ? <p className="p-1 fw-lighter border-bottom">{tasks.length} item left</p> : null}
